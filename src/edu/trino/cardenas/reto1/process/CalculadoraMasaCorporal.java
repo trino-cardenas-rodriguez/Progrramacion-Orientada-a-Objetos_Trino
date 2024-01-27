cc35e00d2a8d97def93377b332191111a5e613b6
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-01-27 00:47:42 -0600

    En este commid estoy subiendo la aplicacion reto1 completa, la cual contiene la calculadora de masa corporal y la interfas de usuario. Contando, ambas, con la documentacion correspondiente.

--- src/edu/trino/cardenas/reto1/process/CalculadoraMasaCorporal.java
+++ src/edu/trino/cardenas/reto1/process/CalculadoraMasaCorporal.java
@@ -6,9 +6,10 @@ package edu.trino.cardenas.reto1.process;
  * Calculo de metabolismo basal
  */
 public class CalculadoraMasaCorporal {
+
     /**
      * Este metodo sirve para realizar el calculo de masa corporal del usuario
-     * con base en el peso y estatura que ingrese
+     * con base en el peso y estatura que ingrese.
      * @param estatura estatura del usuario en centimetros
      * @param peso del usuario en kilogramos
      * @return calculo de masa corporal
@@ -17,26 +18,72 @@ public class CalculadoraMasaCorporal {
         return peso/((estatura/100)*(estatura/100));
     }
 
-    public static String clasificarIndiceMasaCorporal(Double imc) {
+    /**
+     * Este metodo sirve para realizar la clasificaion del estado del usuario;
+     * dependiendo del resultado de su masa corporal,
+     * calculada con el peso y estatura que previamente ingreso.
+     * @param indiceMasaCorporal del usuario (previamente calculado)
+     * @return calculo de masa corporal
+     */
+    public static String clasificarIndiceMasaCorporal(Double indiceMasaCorporal) {
         String value = "";
-        if (imc<18.9){
+        if (indiceMasaCorporal<18.9){
             value = "peso bajo";
-        }else if(imc<=24.99) {
+        }else if(indiceMasaCorporal<=24.99) {
             value = "peso normal";
-        }else if(imc<=29.99) {
+        }else if(indiceMasaCorporal<=29.99) {
             value = "sobrepeso";
-        }else if(imc<=34.99) {
+        }else if(indiceMasaCorporal<=34.99) {
             value = "obesidad leve";
-        }else if(imc<=39.99) {
+        }else if(indiceMasaCorporal<=39.99) {
             value = "obesidad media";
-        }else if(imc>=40.0) {
+        }else if(indiceMasaCorporal>=40.0) {
             value = "obesidad mórbida";
         }
         return value;
     }
 
-    public static double calcularIndiceMasaCorporalMagra(double estatura, double peso){
-        return peso/((estatura/100)*(estatura/100));
+    /**
+     * Este metodo sirve para realizar el calculo de masa corporal magra del usuario
+     * con base en el peso, estatura y sexo que ingrese.
+     * @param estatura estatura del usuario en centimetros
+     * @param peso del usuario en kilogramos
+     * @param sexo del usuario (hombre o mujer)
+     * @return calculo de masa corporal magra
+     */
+    public static double calcularIndiceMasaCorporalMagra(double estatura, double peso, double sexo){
+        double value = 0;
+        if (sexo == 1) {
+            value = (1.10*peso)-128*((peso*peso)/(estatura*estatura));
+        }else if (sexo == 2) {
+            value = (1.07*peso)-148*((peso*peso)/(estatura*estatura));
+        }else {
+            System.out.println("Dato no valido, reinicie");
+        }
+        return value;
+
+    }
+
+    /**
+     * Este metodo sirve para realizar el calculo del metabolismo basal del usuario
+     * con base en el peso, estatura, sexo y edad que ingrese.
+     * @param estatura estatura del usuario en centimetros
+     * @param peso del usuario en kilogramos
+     * @param sexo del usuario (hombre o mujer)
+     * @param edad del usuaro en años
+     * @return calculo de metabolismo basal
+     */
+    public static double calcularIndiceMetabolismoBasal(double estatura, double peso, double sexo, double edad){
+        double value = 0;
+        if (sexo == 1) {
+            value = 66.5+(13.75 * peso)+ (5.003 * estatura)- (6.775 * edad);
+        }else if (sexo == 2) {
+            value = 655.1+(9.563 * peso)+ (1.85 * estatura)- (4.676 * edad);
+        }else {
+            System.out.println("Dato no valido, reinicie");
+        }
+        return value;
+
     }
 }
 
--- src/edu/trino/cardenas/reto1/ui/CLI.java
+++ src/edu/trino/cardenas/reto1/ui/CLI.java
@@ -1,52 +1,118 @@
 package edu.trino.cardenas.reto1.ui;
 
-import edu.trino.cardenas.reto1.process.CalculadoraMasaCorporal;
-
+/**
+ * Aqui se importa el scanner de java para poder reconocer los datos que ingrese el ususario.
+ */
 import java.util.Scanner;
 
-import static edu.trino.cardenas.reto1.process.CalculadoraMasaCorporal.calcularIndiceMasaCorporal;
-import static edu.trino.cardenas.reto1.process.CalculadoraMasaCorporal.clasificarIndiceMasaCorporal;
+/**
+ * Aqui se importa la Calculadora de Masa Corporal del package edu.trino.cardenas.reto1.process,
+ * para realizar los calculos correspondientes a la opcion que elija el usuario;
+ * esto con los datos que ingrese el ususario.
+ */
+import static edu.trino.cardenas.reto1.process.CalculadoraMasaCorporal.*;
 
+/**
+ * Esta clase sirve para mostar el menu al usuario y, en base en la opcion que elija en el menu,
+ * mostrar el resultado del calculo correspondiente a la opcion elegida;
+ * esto en base a los datos que proporcione el usuario.
+ */
 public class CLI {
+
+    /**
+     * Este metodo sirve para mostar el menu al usuario y que pueda elegir la opcion que
+     * corresponda al calculo que decea hacer. (Teniendo una opcion de salir de la aplicacion).
+     * Las opciones son las siguientes:
+     * 1. Calculo de masa corporal
+     * 2. Calculo de masa corporal magra
+     * 3. Calculo de metabolismo basal
+     * 4. Salir
+     */
     public static void showMenu(){
-        System.out.println("--------------");
+        System.out.println("--------------------------------------");
         System.out.println("Elige una opcion de la calculadora");
         System.out.println("1. Calculo de masa corporal");
         System.out.println("2. Calculo de masa corporal magra");
         System.out.println("3. Calculo de metabolismo basal");
-        System.out.println("4. salir");
-        System.out.println("--------------");
+        System.out.println("4. Salir");
+        System.out.println("--------------------------------------");
     }
 
+    /**
+     * Este metodo sirve para inicar la calculadora, mostrando el menu y
+     * leyendo la opcion que seleccione el usuario; y, en base en esa seleccion,
+     * poder pedirle los datos que sean necesarios para el calculo correspondiente
+     * a la opcion que alla elegido; esto utilizando un "switch case".
+     */
     public static void LaunchCalculator(){
         Scanner scanner = new Scanner(System.in);
 
             showMenu();
             int opcion = scanner.nextInt();
-            double estatura, peso;
-            String sexo;
+            double estatura, peso, edad, sexo;
 
+        /**
+         * Este switch lee la opcion que seleccione el usuario y lo redirecciona a la case correspondiente,
+         * dependiendo del calculo que el usuario desea hacer.
+         */
             switch(opcion){
+                /**
+                 * Este case se activa cuando el usuario elige la opcion 1 (Calculo de masa corporal),
+                 * y le solicita al usuario que ingrese los datos necesarios para ese calculo.
+                 */
                 case 1:
                     System.out.println("Ingrese su estatura en cm: ");
                     estatura = scanner.nextDouble();
                     System.out.println("Ingrese su peso en kg: ");
                     peso = scanner.nextDouble();
-                    double imc = calcularIndiceMasaCorporal(estatura, peso);
-                    String estado = clasificarIndiceMasaCorporal(imc);
+                    double indiceMasaCorporal = calcularIndiceMasaCorporal(estatura, peso);
+                    String estado = clasificarIndiceMasaCorporal(indiceMasaCorporal);
                     System.out.printf("Tu indice de masa corporal es %f " +
-                            "por lo que se considera que estas en un estdo de %s",imc,estado);
-
+                            "por lo que se considera que estas en un estdo de %s",indiceMasaCorporal,estado);
                     break;
+                /**
+                 * Este case se activa cuando el usuario elige la opcion 2 (Calculo de masa corporal magra),
+                 * y le solicita al usuario que ingrese los datos necesarios para ese calculo.
+                 */
                 case 2:
-
+                    System.out.println("Ingrese su estatura en cm: ");
+                    estatura = scanner.nextDouble();
+                    System.out.println("Ingrese su peso en kg: ");
+                    peso = scanner.nextDouble();
+                    System.out.println("Ingrese su sexo: \n1. Hombre \n2. Mujer");
+                    sexo = scanner.nextDouble();
+                    double indiceMasaCorporalMagra = calcularIndiceMasaCorporalMagra(estatura, peso, sexo);
+                    System.out.printf("Tu indice de masa corporal magra es: "+indiceMasaCorporalMagra);
                     break;
+                /**
+                 * Este case se activa cuando el usuario elige la opcion 3 (Calculo de metabolismo basal),
+                 * y le solicita al usuario que ingrese los datos necesarios para ese calculo.
+                 */
                 case 3:
-
+                    System.out.println("Ingrese su edad: ");
+                    edad = scanner.nextDouble();
+                    System.out.println("Ingrese su estatura en cm: ");
+                    estatura = scanner.nextDouble();
+                    System.out.println("Ingrese su peso en kg: ");
+                    peso = scanner.nextDouble();
+                    System.out.println("Ingrese su sexo: \n1. Hombre \n2. Mujer");
+                    sexo = scanner.nextDouble();
+                    double indiceMetabolismoBasal = calcularIndiceMetabolismoBasal(estatura, peso, sexo, edad);
+                    System.out.printf("Tu indice de metabolismo basal es: "+indiceMetabolismoBasal);
                     break;
+                /**
+                 * Este case se activa cuando el usuario elige la opcion 4 (salir),
+                 * y permite que el codigo termine, y que el usuario pueda salir de la aplicacion.
+                 */
                 case 4:
-
+                    System.exit(0);
                     break;
+                /**
+                 * Este comando se activa cuando el usuario ingresa un dato no valido o
+                 * una opcion que no se encuentra en el menu.
+                 */
+                default:
+                    throw new IllegalStateException("\nDato no valido: "+opcion+"\nReinicie");
             }
 
         System.out.println("\nAplicación terminada");
