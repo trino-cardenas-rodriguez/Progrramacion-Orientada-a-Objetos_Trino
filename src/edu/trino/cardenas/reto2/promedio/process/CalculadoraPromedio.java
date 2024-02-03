ac5cfe3eb4a3d7a0dd5270b5b68bc443f780fbc0
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-02-02 22:00:56 -0600

    En este commid estoy subiendo la aplicacion promedio completa y documentada; esta aplicacion contiene la calculadora de promedio y la interfaz de usuario, y funciona para dar el promedio de calificaciones dependiendo de los datos que ingrese el usuario.

--- src/edu/trino/cardenas/reto2/promedio/process/CalculadoraPromedio.java
+++ src/edu/trino/cardenas/reto2/promedio/process/CalculadoraPromedio.java
@@ -1,17 +1,19 @@
 package edu.trino.cardenas.reto2.promedio.process;
 
+//Aqui se importa Arrays de java, para poder utilizar los comandos de los arreglos.
 import java.util.Arrays;
 
+//Esta clase sirve para poder calcular el promedio.
 public class CalculadoraPromedio {
+
+    /*Este metodo sirve para realizar el calculo del promedio,
+    con las calificaciones que ingrese el usuario.*/
     public static double calcularPromedio(double[] calificaciones){
+
+        /*Aqui se declara que se van a sumar todas las calificaciones dentro
+        del arreglo "calificaciones", para despues dividir esa suma entre
+        la longitud (cantidad de datos que tiene), del mismo arreglo.*/
         double sumaDeCalificaciones = Arrays.stream(calificaciones).sum();
         return sumaDeCalificaciones/calificaciones.length;
     }
 }
-
-/*
- double sumaDeCalificaciones = 0;
- for (double calificacion : calificaciones){
- sumaDeCalificaciones +=calificacion;
- }
- */
--- src/edu/trino/cardenas/reto2/promedio/ui/CLI.java
+++ src/edu/trino/cardenas/reto2/promedio/ui/CLI.java
@@ -1,22 +1,39 @@
 package edu.trino.cardenas.reto2.promedio.ui;
 
+/*Aqui se importa la calculadora de promedio, del package edu.trino.cardenas.reto2.promedio.process,
+para poder realizar el calculo del promedio con las calificaciones que el usuario ingrese.*/
 import edu.trino.cardenas.reto2.promedio.process.CalculadoraPromedio;
 
+//Aqui se importa el scanner de java para poder reconocer los datos que ingrese el ususario.
 import java.util.Scanner;
 
+/*Esta clase sirve para pedirle al usuario cuantas calificaciones quiere ingresar,
+cuales son esas calificaiones; y, en base en eso, poder regresarle su promedio.*/
 public class CLI {
+
+    /*Este metodo sirve para iniciar la aplicacion pidiendole cuantas calificaciones quiere ingresar,
+    y cuales son esas calificaciones, guardandolas en un arreglo; para despues calcular el
+    promedio de estas.*/
     public static void LaunchApp() {
         Scanner scanner = new Scanner(System.in);
+        //Aqui se lee cuentas calificaciones quiere ingresar el usuario
         System.out.println("Ingrese el numero de calificaciones que desea capturar:");
         int numeroDeCalificaciones = scanner.nextInt();
 
+        /*Aqui se define un arreglo, cuya longitud sera el numero de calificaciones que
+        el usuario quiere ingresar.*/
         double[] calificaciones;
         calificaciones = new double[numeroDeCalificaciones];
+
+        /*Aqui se usa un for para que por cada lugar dentro del arreglo "calificaciones",
+        se pida una calificacion al usuario, guardandolas dentro del arreglo hasta que este se llene.*/
         for (int i = 0; i < numeroDeCalificaciones; i++) {
             System.out.println("Ingrese la calificacion " + (i+1));
             calificaciones[i] = scanner.nextDouble();
         }
 
+        /*Aqui se manda a llamr el metodo CalculadoraPromedio.calcularPromedio(calificaciones),
+        para poder calcular e imprimir el promedio del usuario; en base a los datos que haya ingresado.*/
         double promedio = CalculadoraPromedio.calcularPromedio(calificaciones);
         System.out.println("Su promedio final es: "+promedio);
     }
