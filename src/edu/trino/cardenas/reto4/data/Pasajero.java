c5c93e52ec4774201d25a62edb6cccbcc077d0b7
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-02-17 21:13:10 -0600

    En este commid estoy subiendo las clases Boleto, Ticket y Pasajero, ubicadas en la carpeta data de la aplicacion de Venta de boletos; las tres clases estan completas y documentadas.

--- src/edu/trino/cardenas/reto4/data/Pasajero.java
+++ src/edu/trino/cardenas/reto4/data/Pasajero.java
@@ -1,8 +1,10 @@
 package edu.trino.cardenas.reto4.data;
 
+//Esta clase sirve para declarar la variable nombre y el contructor pasajero.
 public class Pasajero {
     public String nombre;
 
+    //Este contructor guarda la variable de nombre.
     public Pasajero(String nombre) {
         this.nombre = nombre;
     }
--- src/edu/trino/cardenas/reto4/data/Ticket.java
+++ src/edu/trino/cardenas/reto4/data/Ticket.java
@@ -1,7 +1,11 @@
 package edu.trino.cardenas.reto4.data;
 
+//Esta clase permite imprimir el ticket.
 public class Ticket {
 
+    /*Este metodo permite imprimir el ticket que contiene el nombre del comprador,
+    la cantidad de voletos comprados y el importe de la venta,
+    esto llamando variables de la clase Boleto.*/
     public static void imprimirTicket(Boleto boleto) {
         System.out.println("------------Ticket de venta------------");
         System.out.println("Comprador: " + boleto.comprador);
