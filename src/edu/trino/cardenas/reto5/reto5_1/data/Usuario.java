b800aca3e21962569c3d1616991a9d9c5d9a7774
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-03-02 15:36:42 -0600

    En este commit estoy subiendo las clases: Propiedad, Usuario, y BuscadorPropiedad; que plasman en codigo el diagrama del enunciado #1 del reto 5.

---
+++ src/edu/trino/cardenas/reto5/reto5_1/data/Usuario.java
@@ -0,0 +1,10 @@
+package edu.trino.cardenas.reto5.reto5_1.data;
+
+public class Usuario {
+    public double ubicacion;
+
+    public Usuario(double ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+}
