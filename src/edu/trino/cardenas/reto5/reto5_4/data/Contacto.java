4c72c468d61b4c8c701abfae48c95e0465bff884
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-03-02 16:23:36 -0600

    En este commit estoy subiendo las clases: Contacto, y GestorContactos; que plasman en codigo el diagrama del enunciado #4 del reto 5.

---
+++ src/edu/trino/cardenas/reto5/reto5_4/data/Contacto.java
@@ -0,0 +1,16 @@
+package edu.trino.cardenas.reto5.reto5_4.data;
+
+public class Contacto {
+
+    public String nombre;
+    public String direccion;
+    public int telefonoCasa;
+    public int telefonoTrabajo;
+
+    public Contacto(String nombre, String direccion, int telefonoCasa, int telefonoTrabajo) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefonoCasa = telefonoCasa;
+        this.telefonoTrabajo = telefonoTrabajo;
+    }
+}
