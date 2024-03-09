dec94e150bcdb4ad07bf1b93dd611e52590cca7c
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-03-09 13:55:27 -0600

    En este commit estoy subiendo la clase Main del reto 6. Esta clase esta completa y documentada.

---
+++ src/edu/trino/cardenas/reto6/Main.java
@@ -0,0 +1,18 @@
+package edu.trino.cardenas.reto6;
+
+/*Aqui se importa el CLI del package edu.trino.cardenas.reto6.ui
+para poder iniciar la aplicacion*/
+import edu.trino.cardenas.reto6.ui.CLI;
+
+/*Esta clase sirve para llamar el metodo que incia la aplicacion;
+ademas de que muestra un error concreto al usuario en dado caso de
+que ingrese un valor inesperado.*/
+public class Main {
+    public static void main(String[] args) {
+        try{
+            CLI.LaunchApp();
+        }catch (Exception e ){
+            System.out.println("Error inesperado "+e);
+        }
+    }
+}
