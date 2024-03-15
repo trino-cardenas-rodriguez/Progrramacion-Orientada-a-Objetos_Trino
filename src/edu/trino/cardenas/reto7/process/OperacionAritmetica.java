37187c9c1cdb2ceb4d059adc455b2bed4e682b0d
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-03-14 22:55:44 -0600

    En esta commit estoy subiendo un avance de las clases CLI, OperacionAritmetica, Suma, y Resta; del reto #7. Estas clases no estan completas y falta documentarlas.

---
+++ src/edu/trino/cardenas/reto7/process/OperacionAritmetica.java
@@ -0,0 +1,5 @@
+package edu.trino.cardenas.reto7.process;
+
+public abstract class OperacionAritmetica {
+    public abstract int calcular(int valor1, int valor2);
+}
---
+++ src/edu/trino/cardenas/reto7/process/Resta.java
@@ -0,0 +1,8 @@
+package edu.trino.cardenas.reto7.process;
+
+public class Resta extends OperacionAritmetica{
+    @Override
+    public int calcular(int valor1, int valor2) {
+        return valor1 - valor2;
+    }
+}
