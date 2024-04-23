59969fba808f87096f7561ef3239dddb0f3c4cb4
Trino Cárdenas Rodríguez <tacost142@gmail.com>
2024-04-22 21:34:57 -0600

    En este commit estoy subiendo la clase idioma con el factory completo con todas las variables necesarias para imprimir en pantalla dependiendo del idioma que elija el usuario; además estoy subiendo la clase main actualizada. Ambas clases se encuentran documentadas en javadoc.

--- src/edu/trino/cardenas/reto9/Main.java
+++ src/edu/trino/cardenas/reto9/Main.java
@@ -1,12 +1,14 @@
 package edu.trino.cardenas.reto9;
 
-/*Aqui se importa el CLI del package edu.trino.cardenas.reto9.ui
+/**Aquí se importa el CLI del package edu.trino.cardenas.reto9.ui
 para poder iniciar la calculadora.*/
 import edu.trino.cardenas.reto9.ui.CLI;
+
+/**Aquí importamos la clase Idiomas, para usar poder imprimir el error en el idioma que indique el usuario.*/
 import edu.trino.cardenas.reto9.ui.Idiomas;
 
-/*Esta clase sirve para llamar el metodo que incia la aplicacion;
-ademas de que muestra un error concreto al usuario en dado caso de
+/**Esta clase sirve para llamar el método que inicia la aplicación;
+además de que muestra un error concreto al usuario en dado caso de
 que ingrese un valor inesperado.*/
 public class Main {
     public static void main(String[] args) {
--- src/edu/trino/cardenas/reto9/ui/Idiomas.java
+++ src/edu/trino/cardenas/reto9/ui/Idiomas.java
@@ -1,9 +1,9 @@
 package edu.trino.cardenas.reto9.ui;
 
-/*Esta clase sirve para traducir los textos dependiendo del idioma que elija el usuario.*/
+/**Esta clase sirve para traducir los textos dependiendo del idioma que elija el usuario.*/
 public class Idiomas {
 
-    /*Aqui declaramos nuestras variables para definirlas con el idioma que seleccione el usuario.*/
+    /**Aquí declaramos nuestras variables para definirlas con el idioma que seleccione el usuario.*/
     public static String MENU;
     public static String LIBROS_DISPONIBLES;
     public static String El_Hobbit;
@@ -12,12 +12,21 @@ public class Idiomas {
     public static String Libro_1984;
     public static String Rebelion_en_la_granja;
     public static String OPCION_INVALIDA;
+    public static String ARCHIVO_NO_ENCONTRADO;
     public static String ERROR_INESPERADO;
     public static String PALABRAS_MAS_USADAS;
+    public static String VOCALES_TOTALES;
+    public static String PALABRAS_INICIAN_VOCAL;
+    public static String PALABRAS_NUMERO_IMPAR_LETRAS;
+    public static String PALABRA_MAS_LARGA;
+    public static String PALABRA_MAS_CORTA;
+    public static String PALABRA_ESPECIAL;
+    public static String NO_HAY_PALABRAS;
+    public static String CUMPLAN_CONDICION;
     public static String SON;
+    public static String ES;
 
-
-    /*Aqui se define la instancia, dependiendo el idioma que selecciono el usuario.*/
+    /**Aquí se define las variables, dependiendo el idioma que selecciono el usuario.*/
     public static void getInstance(String idioma) {
         Textos opcion = Textos.valueOf(idioma);
         switch (opcion) {
@@ -30,9 +39,20 @@ public class Idiomas {
                 Libro_1984 = "4. 1984.";
                 Rebelion_en_la_granja = "5. Rebelión en la granja.";
                 OPCION_INVALIDA = "Opción inválida";
+                ARCHIVO_NO_ENCONTRADO = "Archivo no encontrado.";
                 ERROR_INESPERADO = "Error inesperado ";
-                PALABRAS_MAS_USADAS = "Las 10 palabras más repetidas en el libro ";
+                PALABRAS_MAS_USADAS = "Las 10 palabras más repetidas en el libro: ";
+                VOCALES_TOTALES = "El numero total de vocales en el libro: ";
+                PALABRAS_INICIAN_VOCAL = "Las palabras que inician con vocal en el libro: ";
+                PALABRAS_NUMERO_IMPAR_LETRAS = "Las palabras con un numero impar de letras en el libro: ";
+                PALABRA_MAS_LARGA = "La palabra mas larga del libro: ";
+                PALABRA_MAS_CORTA = "La palabra mas corta del libro: ";
+                PALABRA_ESPECIAL = "Las palabras que empiecen y terminen con vocal; " +
+                        "y ademas tengan al menos 5 letras, en el libro: ";
+                NO_HAY_PALABRAS = "No se han encontrado palabras dentro del libro: ";
+                CUMPLAN_CONDICION = "que cumplan con esta condicion";
                 SON = " son: ";
+                ES = " es: ";
                 break;
             case Textos.ENG:
                 MENU = "--------------Menu--------------";
@@ -43,16 +63,27 @@ public class Idiomas {
                 Libro_1984 = "4. 1984.";
                 Rebelion_en_la_granja = "5. Rebelión en la granja.";
                 OPCION_INVALIDA = "Invalid option";
+                ARCHIVO_NO_ENCONTRADO = "File not found.";
                 ERROR_INESPERADO = "Unexpected error ";
-                PALABRAS_MAS_USADAS = "Top 10 most repeated words in the book ";
+                PALABRAS_MAS_USADAS = "Top 10 most repeated words in the book: ";
+                VOCALES_TOTALES = "The total number of vowels in the book: ";
+                PALABRAS_INICIAN_VOCAL = "The words that start with a vowel in the book: ";
+                PALABRAS_NUMERO_IMPAR_LETRAS = "The words with an odd number of letters in the book: ";
+                PALABRA_MAS_LARGA = "The longest word in the book: ";
+                PALABRA_MAS_CORTA = "The shortest word in the book: ";
+                PALABRA_ESPECIAL = "The words that begin and end with a vowel; " +
+                        "and also have at least 5 letters in the book: ";
+                NO_HAY_PALABRAS = "No words found within the book: ";
+                CUMPLAN_CONDICION = "that meet this condition";
                 SON = " are: ";
+                ES = " is: ";
                 break;
             default:
                 throw new IllegalArgumentException("Idioma no disponible");
         }
     }
 }
-    /*Aqui definimos las posibles instancias de idioma.*/
+    /**Aqui definimos las posibles instancias de idioma.*/
 enum Textos {
     ESP, ENG
 }
